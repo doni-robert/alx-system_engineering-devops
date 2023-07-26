11d838ed2601abe58d53facc77ede64877faddfe
doni-robert <irobertndungu@gmail.com>
2023-07-26 16:40:28 +0300

    fix output format

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,17 +9,14 @@ import sys
 
 
 if __name__ == "__main__":
-    num_users = len(requests.get(
-                        'https://jsonplaceholder.typicode.com/users/').json())
-    for num in range(1, num_users + 1):
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(num)
-        username = requests.get(url).json().get('username')
-
-        todo = requests.get(url + '/todos/')
-
-        with open('todo_all_employees.json', 'a') as file:
-            json.dump({num: [{
-                "username": username,
-                "task": t.get("title"),
-                "completed": t.get("completed")
-                } for t in todo.json()]}, file)
+    with open('todo_all_employees.json', 'w') as file:
+        users = requests.get(
+                    'https://jsonplaceholder.typicode.com/users').json()
+        todo = requests.get(
+                    'https://jsonplaceholder.typicode.com/todos/').json()
+        json.dump({user.get("id"): [{
+            "username": user.get("username"),
+            "task": t.get("title"),
+            "completed": t.get("completed")
+            } for t in todo if user.get("id") == t.get("userId")]
+            for user in users}, file)
